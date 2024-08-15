187c0e0c084fe0c23a23d1683d3d5d39ed9fa8ff
CristinaOsorio <macry_08@hotmail.com>
2024-08-15 15:30:35 -0600

    feat: config vercel

--- src/app/data/constants.data.ts
+++ src/app/data/constants.data.ts
@@ -1,7 +1,8 @@
+import { environment } from './../../environments/environment';
 
 const IMAGEN_URL = 'https://image.tmdb.org/t/p';
 const API_URL = 'https://api.themoviedb.org/3';
-const APP_URL = 'http://localhost:4200';
+const APP_URL =  environment.appUrl;
 const DB_URL = 'http://localhost:3000';
 
 export { IMAGEN_URL, API_URL, APP_URL, DB_URL };
