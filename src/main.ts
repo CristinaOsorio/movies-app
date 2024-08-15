b8ef06844ecc445ab46998acc36e0d8fe1138c58
CristinaOsorio <macry_08@hotmail.com>
2024-08-15 13:14:14 -0600

    fix: config vercel

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-if (environment.PRODUCTION) {
+if (environment.production) {
   enableProdMode();
 }
 
