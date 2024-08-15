995b9fd70b106f544660f987bbfac6552f432533
CristinaOsorio <macry_08@hotmail.com>
2024-08-15 15:09:42 -0600

    fix: config vercel

--- mynode.js
+++ mynode.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const successColor = '\x1b[32m%s\x1b[0m';
 const checkSign = '\u{2705}';
-const dotenv = require('dotenv').config({path: 'src/environments/.env'}); ;
+const dotenv = require('dotenv').config({path: './src/environments/.env'}); ;
 
 const envFile = `export const environment = {
     production: '${process.env.PRODUCTION}',
@@ -15,6 +15,7 @@ fs.writeFile(targetPath, envFile, (err) => {
         console.error(err);
         throw err;
     } else {
+        
         console.log(successColor, `${checkSign} Successfully generated environment.ts`);
     }
 });
